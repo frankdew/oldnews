bc0c5888dbd6724bb950f1866c6bf74666359503
frankdew <frankdew23@gmail.com>
2016-05-27 17:03:11 +0200

    Change app name

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :news_items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root 'news_items#index'
+
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'news_items#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
